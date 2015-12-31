b0b7253d857b2d3e2820679c06b52fc96fcd2615
twcloudwebapp <twcloudwebapp@outlook.com>
2015-12-31 10:17:29 +0800

    to fix double boots
    it was working on http://taipeibike.azurewebsites.net/boots/boots

--- routes/boots.js
+++ routes/boots.js
@@ -8,8 +8,17 @@ var router = express.Router();
 
 
 /* GET boot page. */
+// to fix double boots
+// http://taipeibike.azurewebsites.net/boots/boots
+//router.get('/boots', function(req, res, next) {
+
+
 router.get('/boots', function(req, res, next) {
-  res.render('index001', { title: '=== Bootstrap ===' });
+
+
+  // res.render('index001', { title: '=== Bootstrap ===' });
+  res.render('boots', { title: '=== Bootstrap ===' });
+
 });
 
 module.exports = router;
